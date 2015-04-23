86a9c55dd825017a38676eea22a71c8dff9ccd11
martin sneath <martinaneath@gmail.com>
2015-04-22 21:09:13 -0400

    fixed the remarks a bit

--- cppcalc.R
+++ cppcalc.R
@@ -1,7 +1,5 @@
 # pension calc
 # http://retirehappy.ca/how-to-calculate-your-cpp-retirement-pension/
-# number of contributory months
-# assuming applying for oct
 birthmonth=09;birthyear=1954
 # my numbers based on 7000 a year for last years estimated then 2000 - basic exemption is $ 3500
 upe=c(245,0,6600,3599,0,4369,8034,5902,6177,0,5000,10714,13600,23400,25800,25900,25123,15157,28900,30500,32200,33400,34400,34900,35400,0,36900,37400,37600,38300,39100,39900,40500,41100,42100,41904,44900,29666,34018,25016,14010,7000,7000,7000,2000,2000,2000,2000,2000)
@@ -12,10 +10,8 @@ ympe=c(5000,5000,5100,5200,5300,5400,5500,5600,6600,7400,8300,9300,10400,11700,1
 retiremonth=birthmonth
 retirestart=birthyear+60
 mb=NULL;ryr=NULL # variables to be used in calculation of life time payments
-
 # start of benefit calculation loop
-for (j in 1:6)
-     
+for (j in 1:6)     
 {
 retireyear=retirestart+j-1 # for this iteration of the loop assume start collecting pension in this year
 ncm=(retireyear-birthyear-18)*12+retiremonth-birthmonth # number contributing months starts when you're 18
@@ -34,7 +30,7 @@ tape1=sum(a$adj)
 m=tape1/ncm
 # general dropout 17 % of APE as of 2014
 gd=round(.17*ncm)
-# remember to look at child years from 1984 - 1993 for kids if spouse doesn't file
+# remember to look at child years for kids if spouse doesn't file
 # any months dropped must be subtracted from ncm
 gd2=gd # once child calc is in make this gd1
 ncm2=ncm
@@ -55,14 +51,11 @@ early=(birthyear+65-retireyear)*12-(birthmonth-retiremonth)
 # reduction for each month early .58 % x months before 65th - goes to .6 2016
 r=.58*early
 if (retireyear>=2016) r=.6*early
-
 m=m-m*r/100;m=round(m,2)
 mb[j]=m;ryr[j]=retireyear
-
 }
-
 # end of benefit calculation loop
-
+# the beginning of the messy stuff - sorry
 z=data.frame(ryr,mb,mb*12,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
 for (i in 1:6)
 {
@@ -74,15 +67,10 @@ c=c("year","monthly","annual","")
 for (i in 5:ncol(z))
      c=c(c,(as.character(retirestart+i-5)))
 colnames(z)=c
-
 write.csv(z,"cppbreakeven.csv")
-
 # to do
 # what happens when you go over 65 - reverse early calc, then when you pass 70
 # to make it work for people born before 1948 must put in hard 1966
 # add more ympe figures - it stops at 2020 now - do it at a % based on the last one
-# child dropout - i am not eligible - did not get family allowance cheque
-# read spreadsheet of pensionable earnings, birth, retirement start, % cola and expected pension
-
 # disability months
 
